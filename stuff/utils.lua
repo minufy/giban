eac71d2e4784bf276805136ee01395ceede5cca1
minu <alsdn638@gmail.com>
2025-12-10 20:34:34 +0900

    포매팅 수정

--- stuff/utils.lua
+++ stuff/utils.lua
@@ -32,10 +32,10 @@ function Sign(x)
 end
 
 function AABB(a, b)
-  return a.x < b.x+b.w and
-         b.x < a.x+a.w and
-         a.y < b.y+b.h and
-         b.y < a.y+a.h
+    return a.x < b.x+b.w and
+           b.x < a.x+a.w and
+           a.y < b.y+b.h and
+           b.y < a.y+a.h
 end
 
 function NewImage(name)
