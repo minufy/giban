c46355536c77284e5206896d82d381601b5a7295
minu <alsdn638@gmail.com>
2025-12-28 21:13:56 +0900

    camera default dmaping

--- objects/player.lua
+++ objects/player.lua
@@ -13,12 +13,17 @@ function Player:init(x, y)
     self.w = img:getWidth()
     self.h = img:getHeight()
 
-    Camera:offset(Res.w/2, Res.h/2)
-    Camera:set(self.x, self.y)
-    Camera:snap_back()
+    self.camera_init = false
 end
 
 function Player:update(dt)
+    if not self.camera_init then
+        self.camera_init = true
+        Camera:offset(Res.w/2, Res.h/2)
+        Camera:set(self.x, self.y)
+        Camera:snap_back()
+    end
+
     Camera:set(self.x, self.y)
     if Input.right.down then
         self:move_x(4*dt)
